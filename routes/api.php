bc11c3a170b26c0efe2721c4b5de216e8d8f91e5
pawas007 <trushostap@gmail.com>
2023-03-07 18:49:03 +0200

    Fix route methods

--- routes/api.php
+++ routes/api.php
@@ -20,19 +20,19 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::prefix('notifications')->group(function () {
         Route::get('', [NotificationsController::class, 'index']);
-        Route::get('{id}/mark/toggle', [NotificationsController::class, 'markAsRead']);
-        Route::get('{id}/destroy', [NotificationsController::class, 'destroy']);
+        Route::put('{id}/mark/toggle', [NotificationsController::class, 'markAsRead']);
+        Route::delete('{id}/destroy', [NotificationsController::class, 'destroy']);
     });
 
     Route::prefix('friend')->group(function () {
         Route::get('/', [FriendController::class, 'friend']);
         Route::get('pending', [FriendController::class, 'friendPending']);
-        Route::get('pending/{user}/cansel', [FriendController::class, 'friendPendingCansel']);
+        Route::delete('pending/{user}/cansel', [FriendController::class, 'friendPendingCansel']);
         Route::get('request', [FriendController::class, 'friendRequest']);
-        Route::get('request/{user}/accept', [FriendController::class, 'friendAccept']);
-        Route::get('request/{user}/decline', [FriendController::class, 'friendDecline']);
-        Route::get('{user}/add', [FriendController::class, 'friendAdd']);
-        Route::get('{user}/delete', [FriendController::class, 'friendDelete']);
+        Route::post('request/{user}/accept', [FriendController::class, 'friendAccept']);
+        Route::delete('request/{user}/decline', [FriendController::class, 'friendDecline']);
+        Route::post('{user}/add', [FriendController::class, 'friendAdd']);
+        Route::delete('{user}/delete', [FriendController::class, 'friendDelete']);
         Route::get('counts', [FriendController::class, 'friendCounts']);
     });
 
