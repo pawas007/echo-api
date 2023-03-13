bebded00b0a33ba20dcbdd3fe8db92cf129b60ff
pawas007 <trushostap@gmail.com>
2023-03-13 23:40:54 +0200

    Refactor seeder

--- database/seeders/SingleUserSeeder.php
+++ database/seeders/SingleUserSeeder.php
@@ -19,13 +19,14 @@ class SingleUserSeeder extends Seeder
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'phone' => $faker->phoneNumber,
+            'phone' => substr($faker->e164PhoneNumber, 1),
         ]);
         $user->profile()->create([
             'country' => $faker->country,
             'age' => rand(10, 109),
             'poster' => null,
             'address' => $faker->address,
+            'sex' => 'Male',
             'about' => $faker->text(20),
             'quote' => $faker->text(5),
             'birthday' => $faker->dateTime
